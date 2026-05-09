2920b1006b9dfe327387bb3338af211ea116f166
amanjeetkumar22 <amanjeetkumar31@gmail.com>
2026-05-09 22:57:30 +0530

    push

--- file/line_no.py
+++ file/line_no.py
@@ -2,29 +2,28 @@
 words=["Aman","Jeet","Kalwar"]
 with open ("line.txt","r") as f:
     content=f.read()
-
 ##to search ->"Word" in text file 
 # if("python" in content):
 #     print("Yes found")
 # else:
 #     print("not found")
+    
 
 ##to replace word that is stored in list with # 
 # for word in words:
 #     content=content.replace(word,"#"*len(word))
 
-# with open("line.txt","r")as f:
-#     content=f.read()
-#     if("###" in content):
-#         content=content.replace("###",words)
+##replacing words with "#" times of length of word 
+for word in words:
+    content=content.replace(word,"#"*len(word))
 
-
+##change in word number 
 # for i, word in enumerate(words):
-#     content = content.replace(word, f"#WORD{i}#")
+    # content = content.replace(word,f"#WORD{i}#")
 
-
-for i, word in enumerate(words):
-    content = content.replace(f"#WORD{i}#", word)
+##again change in word     
+# for i, word in enumerate(words):
+    # content = content.replace(f"#WORD{i}#",word)
 
 
 with open("line.txt","w") as f:
